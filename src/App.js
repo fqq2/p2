7136b916d3b542bafcaa080aadca650d55a47c05
Franz <pfquispe@gmail.com>
2020-04-11 20:53:40 -0400

    Added main app.

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import axios from 'axios';
+
+import PageTabs from './PageTabs';
+import GridView from './GridView';
+
+class App extends React.Component {
+    state = {
+        view: 'grid',
+        allTasks: [],
+        sortedTasks: {
+            todo: [],
+            inProgress: [],
+            review: [],
+            done: []
+        },
+        errorText: ''
+    }
+
+    componentDidMount() {
+        this.getTasks();
+    }
+
+    getTasks() {
+        axios.get(`http://my-json-server.typicode.com/fqq2/db/posts`)
+            .then(response => {
+                this.setState({ allTasks: response.data, sortedTasks: this.sortTasks(response.data) });
+            }).catch (error => {
+            this.setState({ errorMessage: error.message });
+        });
+    }
+
+    sortTasks(tasks) {
+        return {
+            todo: tasks.filter(post => post.column === 'todo'),
+            inProgress: tasks.filter(post => post.column === 'in-progress'),
+            review: tasks.filter(post => post.column === 'review'),
+            done: tasks.filter(post => post.column === 'done'),
+        }
+    }
+
+    onUpdateTask(_task) {
+        let allTasks = this.state.allTasks;
+        const index = allTasks.findIndex(task => task.id === _task.id);
+        allTasks[index] = _task;
+
+        const sortedTasks = this.sortTasks(allTasks);
+        this.setState({ allTasks, sortedTasks })
+    }
+
+    onViewChange(view) {
+        this.setState({ view });
+    }
+
+    wrapPage(jsx) {
+        const { view } = this.state;
+        return (
+            <div className="container">
+            <PageTabs currentView={view}
+        onViewChange={this.onViewChange.bind(this)}/>
+        {jsx}
+    </div>
+    );
+    }
+
+    render() {
+        const { view } = this.state;
+
+        switch (view) {
+            case 'grid':
+                return (this.wrapPage(
+                    <GridView tasks={this.state.sortedTasks} onUpdateTask={(task)=> this.onUpdateTask(task)} />
+    ));
+    }
+
+    }
+}
+
+export default App;
+
