f8c8b08011bd3cb945bb454eada2b874744889da
Franz <pfquispe@gmail.com>
2020-04-13 11:01:20 -0400

    Updates

---
+++ precache-manifest.029f26ed607769c5e124dc7b9a68afbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7522b68d2a89c6d224831508664b870",
+    "url": "/p2/index.html"
+  },
+  {
+    "revision": "0ad90ac8d7cb88ce2363",
+    "url": "/p2/static/js/2.a6374f57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/p2/static/js/2.a6374f57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5803c80f72951e0b322",
+    "url": "/p2/static/js/main.cde48aec.chunk.js"
+  },
+  {
+    "revision": "33b06656efa6f3385c02",
+    "url": "/p2/static/js/runtime-main.9eef335c.js"
+  }
+]);
